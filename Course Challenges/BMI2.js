f9f723f7d5e8feac4e4d1e914d4b5ea4f2f1d12e
kevalkennect <keval.panchal@kennect.io>
2022-07-15 11:33:57 +0530

    BMI 2

---
+++ Course Challenges/BMI2.js
@@ -0,0 +1,25 @@
+const mark = {
+  fullName: "Mark",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+const john = {
+  fullName: "John",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+console.log(
+  mark.calcBMI() > john.calcBMI()
+    ? `Mark's BMI (${mark.BMI}) is higher than John's (${john.BMI})!`
+    : `John's BMI (${john.BMI}) is higher than Mark's (${mark.BMI})!`
+);
---
+++ Course Challenges/tip2.js
@@ -0,0 +1,10 @@
+var arr = [125, 55, 44];
+var tip = [];
+const calcTip = (value) =>
+  value >= 50 && value <= 300 ? (value * 15) / 100 : (value * 20) / 100;
+
+for (let index = 0; index < arr.length; index++) {
+  tip.push(calcTip(arr[index]));
+}
+// const tip = [calcTip(arr[0]), calcTip(arr[1]), calcTip(arr[2])];
+console.log(tip);
