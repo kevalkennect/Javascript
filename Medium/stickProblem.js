bf3b50c71b577d5474b5c7cca84de62ecb8fd62a
kevalkennect <keval.panchal@kennect.io>
2022-07-15 16:23:43 +0530

    changes

--- Medium/stickProblem.js
+++ Medium/stickProblem.js
@@ -1,7 +1,13 @@
-const numberOfStick = 6;
 
 function matchHouses(step) {
-  return step * numberOfStick - (step - 1);
-}
-
+    if (step === 0) {
+      return 0
+    } else if (step < 0 || typeof step !== "number") {
+      return "The input (step) will always be a non-negative integer."
+    } else {
+          return (step * 6) - (step - 1);
+      }
+  }
+  
+  
 console.log(matchHouses(87))
