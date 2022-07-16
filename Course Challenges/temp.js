8c94225f55aa2982ac36a078f003145721463400
kevalkennect <keval.panchal@kennect.io>
2022-07-16 15:29:25 +0530

    Temp

---
+++ Course Challenges/temp.js
@@ -0,0 +1,8 @@
+function printForecast(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const currentValue = i + 1;
+    console.log(`${arr[i]}ºC in ${currentValue} days`);
+  }
+}
+
+printForecast([21, -2, 45])
