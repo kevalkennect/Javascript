687e02de36164e8b8dced0c9aacf919b6b7b7564
kevalkennect <keval.panchal@kennect.io>
2022-07-19 15:54:26 +0530

    football3

---
+++ Course Challenges/football3.js
@@ -0,0 +1,20 @@
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "� Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "� Substitution"],
+  [64, "� Yellow card"],
+  [69, "� Red card"],
+  [70, "� Substitution"],
+  [72, "� Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "� Yellow card"],
+]);
+
+const event = [...new Set(gameEvents.values())];
+
+gameEvents.delete(64);
+
+console.log(`An event happened, on
+average, every ${90 / gameEvents.size} minutes`);
